d60fb5c4511776f8395fb2fd0c13cb55cd359bb7
Sai Hlaing <saisailuhlaing@gmail.com>
2023-09-28 10:00:29 +0630

    Add files via upload

--- go-lang/main.go
+++ go-lang/main.go
@@ -8,31 +8,48 @@ import (
 )
 
 const (
-	topic          = "message-log"
 	broker1Address = "localhost:9092"
 	broker2Address = "localhost:9093"
 	broker3Address = "localhost:9094"
 )
 
+func producer(ctx context.Context) {
+	config := kafka.WriterConfig{
+		Brokers: []string{broker1Address, broker2Address, broker3Address},
+	}
+
+	writer := kafka.NewWriter(config)
+
+	err := writer.WriteMessages(ctx, kafka.Message{Topic: "user", Value: []byte("Hello")})
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+}
+
+// func consumer(ctx context.Context) {
+// 	config := kafka.ReaderConfig{
+// 		Brokers:  []string{broker1Address, broker2Address, broker3Address},
+// 		Topic:    "user",
+// 		GroupID:  "user-consumer",
+// 		MaxBytes: 10,
+// 	}
+
+// 	reader := kafka.NewReader(config)
+
+// 	for {
+// 		m, err := reader.ReadMessage(context.Background())
+// 		if err != nil {
+// 			fmt.Println("Some error occured", err)
+// 			break
+// 		}
+// 		fmt.Println("Message is : ", string(m.Value))
+// 	}
+// }
+
 func main() {
-	conf := kafka.ReaderConfig{
-		Brokers:  []string{broker1Address, broker2Address, broker3Address},
-		Topic:    "user",
-		GroupID:  "user-consumer",
-		MaxBytes: 10,
-	}
-
-	reader := kafka.NewReader(conf)
-
-	fmt.Println("Start listening")
-
-	for {
-		m, err := reader.ReadMessage(context.Background())
-		if err != nil {
-			fmt.Println("Some error occured", err)
-			break
-		}
-		fmt.Println("Message is : ", string(m.Value))
-	}
-
+	fmt.Println("Start Producing")
+	// for range []int{1, 2, 3} {
+	producer(context.Background())
+	// }
+	// consumer(context.Background())
 }
